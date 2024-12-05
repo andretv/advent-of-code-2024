f0a2cb8a07ad2395f99ffbbf9cea93190a1cfeff
André Vaccari <andrevaccari95@gmail.com>
2024-12-05 17:27:52 -0300

    Day 5 solution.

---
+++ days/day_5/src/part_2.rs
@@ -0,0 +1,83 @@
+#[derive(Debug)]
+struct Rule {
+    before: u8,
+    after: u8,
+}
+
+pub fn solution(input: &str) -> u32 {
+    let mut input_iter = input.lines();
+    let mut rules: Vec<Rule> = vec![];
+    let mut updates: Vec<Vec<u8>> = vec![];
+
+    // Rules
+    while let Some(line) = input_iter.next() {
+        if line.is_empty() {
+            break;
+        }
+
+        let split: Vec<&str> = line.split("|").collect();
+        let before: u8 = split[0]
+            .parse()
+            .expect("Before needs to be a parsable number.");
+        let after: u8 = split[1]
+            .parse()
+            .expect("After needs to be a parsable number.");
+
+        rules.push(Rule { before, after });
+    }
+
+    // Updates
+    while let Some(line) = input_iter.next() {
+        let update: Vec<_> = line
+            .split(",")
+            .map(|n| {
+                n.parse::<u8>()
+                    .expect("Update character needs to be a parsable number.")
+            })
+            .collect();
+        updates.push(update);
+    }
+
+    updates
+        .iter_mut()
+        .filter_map(|update| {
+            // Iter over each number.
+            for (idx, number) in update.iter().enumerate() {
+                // Checks the rules for that number.
+                for rule in rules.iter().filter(|r| &r.before == number) {
+                    // Checks every number before this number.
+                    for i in 0..update[0..idx].len() {
+                        if update[i] == rule.after {
+                            fix_update(update, &rules);
+                            let middle_idx = ((update.len() + 1) / 2) - 1;
+                            return Some(u32::from(update[middle_idx]));
+                        }
+                    }
+                }
+            }
+
+            return None;
+        })
+        .sum()
+}
+
+/// Swap vector numbers until they are agreeing with the rules.
+fn fix_update(update: &mut Vec<u8>, rules: &Vec<Rule>) {
+    let mut idx = 0;
+
+    'outer: while idx < update.len() {
+        let number = &update[idx];
+
+        for rule in rules.iter().filter(|r| &r.before == number) {
+            for i in 0..update[0..idx].len() {
+                if update[i] == rule.after {
+                    update.swap(i, idx);
+                    idx = 0;
+                    continue 'outer;
+                }
+            }
+        }
+
+        idx += 1;
+    }
+}
